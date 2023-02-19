d3714ec089e8749cfcd302723509666ff6598557
KA1PE5H <kalpeshvish7@gmail.com>
2023-02-19 11:14:50 +0530

    sixth commit

--- data/quickfood.js
+++ data/quickfood.js
@@ -1,32 +1,32 @@
 export default [
     {
         id:"0",
-        image:"https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_264,h_288,c_fill/w1rrow27w3vb3ur5xbi9",
+        image:"https://raw.githubusercontent.com/KA1PE5H/frocker-native-main/main/assets/sneak1.png?token=GHSAT0AAAAAAB65ZMCRAWPVYQANKGFD3DS2Y7RYSZQ",
         views:"50",
         description:"The best salad bowl from the best in town"
     },
     {
         id:"1",
-        image:"https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_264,h_288,c_fill/ch07avzpos83wrfdfhyh",
+        image:"https://raw.githubusercontent.com/KA1PE5H/frocker-native-main/main/assets/sneak2.png?token=GHSAT0AAAAAAB65ZMCRDEPKSPS75U56FQQOY7RYT7Q",
         views:"50",
         description:"The best salad bowl from the best in town"
 
     },
     {
         id:"2",
-        image:"https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_264,h_288,c_fill/hiezygiuixjqjogg6w2b",
+        image:"https://raw.githubusercontent.com/KA1PE5H/frocker-native-main/main/assets/sneak3.png?token=GHSAT0AAAAAAB65ZMCQI4KU3KKUG6C4JBROY7RYUJA",
         views:"40",
         description:"The best salad bowl from the best in town",
     },
     {
         id:"3",
-        image:"https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_264,h_288,c_fill/q391dhemwszg9orhgldh",
+        image:"",
         views:"30",
         description:"The best salad bowl from the best in town"
     },
     {
         id:"4",
-        image:"https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_112,h_112,c_fill/ei26sjhizcjrk9gmjy5r",
+        image:"",
         views:"20",
         description:"60%"
     }
